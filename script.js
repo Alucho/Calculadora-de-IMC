abc72f97b2c0289201cef240bf86a40d0ebfa0b2
Anderson Lucho <63527795+Alucho@users.noreply.github.com>
2022-03-31 16:25:52 +0000

    atualização

--- script.js
+++ script.js
@@ -4,29 +4,30 @@ function calcular(){
   let altura = +formulario.altura.value;
   let peso = +formulario.peso.value;
   let imc = peso / (altura ** 2);
-
+  let resultado;
+  
   if(imc < 20){
-    resultado = ("Abaixo do peso!");
+    resultado = (" Abaixo do peso!");
   } 
   else if(imc >20 && imc <= 25){
-    resultado = ("Peso Ideal");
+    resultado = (" Peso Ideal");
   }
   else if(imc >25 && imc <= 30){
-    resultado = ("Sobrepeso");
+    resultado = (" Sobrepeso");
   }
   else if(imc >30 && imc <= 35){
-    resultado = ("Obesidade Moderada");
+    resultado = (" Obesidade Moderada");
   }
   else if(imc >35 && imc <= 40){
-    resultado = ("Obesidade Severa");
+    resultado = (" Obesidade Severa");
   }
   else if(imc >40 && imc <= 50){
-    resultado = ("Obesidade Morbida");
+    resultado = (" Obesidade Morbida");
   }
   else{
    resultado = ('Falha de cálculo!');
   }
 
-  formulario.imc.value = imc.toFixed(2)
+  formulario.imc.value = imc.toFixed(2) + resultado
   
 }
